2f352336ceff6358bc7f7ac56fb78b18b4d76a6f
Dencho96 <denysvk.96@gmail.com>
2020-10-04 13:01:26 +0300

    Travel-Agency Start

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    loop: 'true',
+    spaceBetween: 30,
+    autoplay: {
+        delay: 5000,
+      },
+    direction: 'horizontal',
+});
