84897265977f0df28f05222864427e99bd7fcaf2
Dencho96 <denysvk.96@gmail.com>
2020-10-04 15:18:16 +0300

    added testimonials

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-var mySwiper = new Swiper('.swiper-container', {
+var mySwiper = new Swiper('.hot__slider', {
     slidesPerView: 3,
     loop: 'true',
     spaceBetween: 30,
@@ -6,4 +6,15 @@ var mySwiper = new Swiper('.swiper-container', {
         delay: 5000,
       },
     direction: 'horizontal',
+});
+
+var mySwiper1 = new Swiper('.testimonials__slider', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: 'true',
+  direction: 'horizontal',
+  navigation: {
+    nextEl: '.testimonials__btn--prev',
+    prevEl: '.testimonials__btn--next',
+  },
 });
